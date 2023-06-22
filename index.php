1e42b4aaac137e9b8c3a010d5c986a084821dbd1
Salvatore-94 <57541880+Salvatore-94@users.noreply.github.com>
2023-06-22 15:41:11 +0200

    Mod

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+include('header.php');
+
+// Conessione DB
+$conn = mysqli_connect('localhost', 'sitidev_salrubrica', 'i3@vV3_r8WnG', 'sitidev_salrubrica');
+
+$query = "SELECT * FROM rubrica";
+
+$risultati = $conn->query($query);
+
+
+
+while ($row = $risultati->fetch_assoc()) {
+    $final_ary[] = $row;
+}
+?>
+<div class="container">
+
+<table class="table table-hover">
+    <tr>
+        <td>Nomeeee</td>
+        <td>Cognome</td>
+        <td>Numero</td>
+        <td>Email</td>
+        <td>Sesso</td>
+    </tr>
+
+    <?php
+    foreach ($final_ary as $k => $v) {
+        $nome = $v['nome'];
+        $cognome = $v['cognome'];
+        $numero_tel = $v['numero_tel'];
+        $email = $v['email'];
+        $sesso = $v['sesso'];
+
+    ?>
+        <tr>
+            <td><?php echo $nome ?></td>
+            <td><?php echo $cognome ?></td>
+            <td><?php echo $numero_tel ?></td>
+            <td><?php echo $email ?></td>
+            <td><?php echo $sesso ?></td>
+        </tr>
+
+    <?php
+
+    }
+
+    ?>
+</table>
+</div>
+
+<a href='http://salvatore.sitidev.it/rubrica.php'> Aggiungi</a>
+
+<?php
+include('footer.php'); ?>
